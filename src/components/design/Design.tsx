8d78ade3d72eb586c5391cd8d585174c12bebcaf
mlprolo <mauricio.prolo@gmail.com>
2026-06-21 15:55:11 -0300

    Update Design.tsx

--- src/components/design/Design.tsx
+++ src/components/design/Design.tsx
@@ -5,10 +5,42 @@ function Design() {
       <h3>Design Gráfico</h3>
       <img src="../../public/img/Gimp.png" style={{ width: '300px', maxWidth: '100%' }} />
       <h4>Ementa</h4>
-      <p>Princípios da comunicação visual aplicados ao meio digital. Tipografia. Cor. Imagemdigital: dimensão, resolução e formatos. Construção de imagens e composições. Usodesoftware para design gráfico e edição de imagens digitais.
+      <p>Princípios da comunicação visual aplicados ao meio digital. Tipografia. Cor. Imagemdigital: dimensão, resolução e formatos. Construção de imagens e composições.
+        Uso de software para design gráfico e edição de imagens digitais.
       </p>
       <h4>Duração</h4>
       <p>30h</p>
+
+      <h4>Objetivos de Aprendizagem</h4>
+      <ul>
+        <li>Capacitar os alunos a utilizar ferramentas de prototipagem e design gráfico para criar projetos visuais.</li>
+        <li>Compreender e aplicar os princípios fundamentais da comunicação visual e do design responsivo em projetos práticos de layout.</li>
+        <li>Manipular imagens digitais controlando suas dimensões, resoluções, formatos e ajustes fundamentais como brilho e contraste.</li>
+        <li>Desenvolver interfaces e protótipos de layouts criativos e inovadores para aplicações móveis e desktop, combinando de forma harmônica tipografia, cores e elementos gráficos.</li>
+      </ul>
+
+      <h4>Conteúdos:</h4>
+      <ul>
+        <li>
+          <strong>Princípios de Comunicação Visual:</strong> Elementos fundamentais do design (linhas, formas, cores, texturas e tipografia).
+          Aplicação prática de conceitos de equilíbrio, contraste, hierarquia, proporção, unidade e ênfase em layouts.
+        </li>
+        <li>
+          <strong>Imagem Digital e Edição:</strong> Conceitos de dimensão, resolução e formatos de imagens digitais. Apresentação e uso de software de
+          edição de imagens com suas ferramentas de seleção, corte, redimensionamento, ajuste de brilho e contraste.
+        </li>
+        <li>
+          <strong>Prototipagem e Interação:</strong> Introdução à prototipagem de interfaces e criação de protótipos-layout. Configuração de ferramentas
+          específicas e uso da paleta de componentes para o desenvolvimento de páginas Web e interfaces de dispositivos móveis.
+          Criação de transições, efeitos para usabilidade e práticas de trabalho colaborativo em equipe (compartilhamento, comentários e revisões).
+        </li>
+      </ul>
+
+      <h4>Ferramentas e Tecnologias</h4>
+      <ul>
+        <li><strong>Software de Edição de Imagens Digitais</strong>:Ferramenta dedicada ao tratamento, corte, ajuste e manipulação técnica de elementos visuais e composições.</li>
+        <li><strong>Ferramenta de Prototipação</strong>: Plataforma digital voltada à construção de componentes, desenho de layouts responsivos para web/mobile e criação de interações e transições.</li>
+      </ul>
 
     </>
   )
